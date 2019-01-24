eb4631e992c1d89c7962ae508273158540ce73b9
Alex <Alex@10.97.59.102>
2019-01-24 14:24:58 -0500

    Removed Documents Folder & Changed singles panel to proper size

--- TournamnetTracker/src/singles.java
+++ TournamnetTracker/src/singles.java
@@ -9,18 +9,18 @@ public class singles extends JPanel {
 	 * Create the panel.
 	 */
 	public singles() {
-		setBounds(0,25,440,765);
+		setBounds(0,30,440,740);
 		setLayout(null);
 		setFocusable(false);
 		
 		textField = new JTextField();
-		textField.setBounds(52, 254, 86, 20);
+		textField.setBounds(354, 0, 86, 20);
 		add(textField);
 		textField.setColumns(10);
 		
 		JLabel lblNewLabel = new JLabel("New label");
 		lblNewLabel.setFocusable(false);
-		lblNewLabel.setBounds(52, 229, 46, 14);
+		lblNewLabel.setBounds(0, 726, 363, 14);
 		add(lblNewLabel);
 		
 		Thread t = new Thread(new Runnable() { public void run() { 
